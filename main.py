a6669edb11bc9b083b0b623ac07424c967079099
Gautam Singh Mahara <107118978+Gautam0612@users.noreply.github.com>
2024-04-24 23:35:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,10 @@ db = client['userData']
 collection = db['Data']
 
 
+@app.route("/",methods=['POST'])
+def demo():
+     return jsonify({'message': "Username not available"})
+
 @app.route("/signup", methods=['POST'])
 def sign():
     try:
